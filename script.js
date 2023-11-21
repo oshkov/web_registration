05abf1996aab9235b147eae9c7b150f3c56120a4
Alexander Oshkov <sasha.oshkov03@gmail.com>
2023-11-21 22:32:56 +0300

    fixed css

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ let tg = window.Telegram.WebApp
 let registrate_btn = document.getElementById('registrate')
 
 registrate_btn.addEventListener('click', () => {
-    let user_name = document.getElementById('user_name')
-    let last_name = document.getElementById('last_name')
-    let phone = document.getElementById('phone')
+    let user_name = document.getElementById('user_name').value;
+    let last_name = document.getElementById('last_name').value;
+    let phone = document.getElementById('phone').value;
 
     let data = {
-        user_name: user_name,
-        last_name: last_name,
-        phone: phone
+        имя: user_name,
+        фамилия: last_name,
+        телефон: phone
     }
 
-    tg.sendData(JSON.stringify(data))
+    tg.sendData(JSON.stringify(data));
 
-    tg.close()
+    tg.close();
 })
