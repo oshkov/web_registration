1ce00d5d0227c1af92d21a1a4624821b8d2e85d9
Alexander Oshkov <sasha.oshkov03@gmail.com>
2023-11-22 22:20:18 +0300

    test mini-quiz

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 let tg = window.Telegram.WebApp
+let start_registrate_btn = document.getElementById('start_registrate')
+let username_btn = document.getElementById('continue_username')
+let lastname_btn = document.getElementById('continue_lastname')
 let registrate_btn = document.getElementById('registrate')
+
+start_registrate_btn.addEventListener('click', () => {
+    document.getElementById('start_registration').style.display = 'none'
+    document.getElementById('username_form').style.display = 'block'
+})
+
+username_btn.addEventListener('click', () => {
+    document.getElementById('username_form').style.display = 'none'
+    document.getElementById('lastname_form').style.display = 'block'
+})
+
+lastname_btn.addEventListener('click', () => {
+    document.getElementById('lastname_form').style.display = 'none'
+    document.getElementById('phone_form').style.display = 'block'
+})
 
 registrate_btn.addEventListener('click', () => {
     let user_name = document.getElementById('user_name').value;
